bdd8ba9e6777693a85145bb0ba7230d5b3144331
abubakrali10 <bakrali175@yahoo.com>
2023-06-25 07:55:12 -0700

    sorting array using bubble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+void swap(int *a, int *b);
+
+/**
+ * bubble_sort - sorting an array using bubble sort algo
+ * @array: pinter to array to be sorted
+ * @size: size of given array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int flag = 0;
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+				flag = 1;
+			}
+		}
+
+		if (flag == 0)
+			break;
+	}
+}
+
+/**
+ * swap - swapping two elements
+ * @a: pointer to first element to swap
+ * @b: pointer to second element to swap
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
